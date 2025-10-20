440d6d45e92860ca93c7775f3294e554796ffdeb
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-10-20 11:12:12 +0530

    Create solution.sql

---
+++ 09-Rising-Temperature/solution.sql
@@ -0,0 +1,6 @@
+-- # Write your MySQL query statement below
+SELECT a.id
+FROM Weather AS a
+JOIN Weather AS b
+WHERE DATEDIFF(a.recordDate, b.recordDate) = 1 AND a.temperature > b.temperature 
+ORDER BY a.recordDate ASC;
