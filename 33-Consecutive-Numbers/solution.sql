dea38e89acccb3b4fac7a25607897dbb4a9c0d6c
Snehasish Mukherjee <snehasish1612@gmail.com>
2026-01-18 17:20:21 +0530

    Create solution.sql

---
+++ 33-Consecutive-Numbers/solution.sql
@@ -0,0 +1,7 @@
+-- # Write your MySQL query statement below
+SELECT DISTINCT l1.num AS ConsecutiveNums 
+FROM Logs AS l1
+JOIN Logs AS l2
+JOIN Logs AS l3
+ON l1.id = l2.id - 1 AND l1.id = l3.id - 2
+WHERE l1.num = l2.num AND l2.num = l3.num;
